073389a8592ea7e3f162092ffca7b2b83bd5a251
ddcong <daodinhcong2000@gmail.com>
2022-02-14 12:58:07 +0700

    init

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Happy birtday",
+    introTitle: 'DICO <3 ',
+    introDesc: `Bạn sẵn sàng gửi những lời chúc tốt đẹp đến bạn Công đẹp trai chứ 😘😘 `,
+    btnIntro: '^^Đồng ý^^',
+    title: 'Bạn luôn luôn muốn gửi những lời chúc tốt đẹp đến Công phải không 🥰',
+    desc: 'Hãy thực hiện điều đó ngay bây giờ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Hãy dành những lời chúc thân thương nhất của bạn cho mình nhé <3',
+    btnReply: 'Gửi cho Công <3',
+    reply: 'Công là một người bạn tuyệt vời, một vài câu không thể nói hết về bạn <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Nhớ gửi quà sinh nhật qua stk ngân hàng cho mình nhá.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/dinhcong2k' //link mess của các bạn. VD: https://m.me/dinhcong2k
+}
